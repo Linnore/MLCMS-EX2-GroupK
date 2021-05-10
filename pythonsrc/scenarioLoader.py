0ce09fe5ff76ea703763c3c3d665fdcc06095e8f
Linnore <junleli@link.cuhk.edu.cn>
2021-05-10 19:21:54 +0200

    Documented

--- pythonsrc/scenarioLoader.py
+++ pythonsrc/scenarioLoader.py
@@ -2,17 +2,24 @@ import json
 import numpy as np
 import os
 
+
 class scenarioLoader:
     """
     This class loads the given scenario file and supports adding pedestrian at a given position
     """
 
     def __init__(self, scenario_file):
+        """Initialize a scenarioLoader object
 
+        Args:
+            scenario_file (string): The path to a vadere scenario file.
+        """
         self.scenario_file = scenario_file
         self.load()
 
     def load(self):
+        """Load the vadere scenario file from json format into a dictionary
+        """
         with open(self.scenario_file, "r") as myScenario:
             self.scenario_file_dict = json.load(myScenario)
 
@@ -20,6 +27,14 @@ class scenarioLoader:
             "scenario"]["topography"]["dynamicElements"]
 
     def addPed(self, x=0, y=0, speed=np.random.normal(1.34, 0.26), targetIDs=[]):
+        """Add a pedestrian into the dynamicElements list.
+
+        Args:
+            x (int, optional): x coordinate of the pedestrian. Defaults to 0.
+            y (int, optional): y coordinate of the pedestrian. Defaults to 0.
+            speed (float, optional): The freeFlowSpeed of the pedestrian. Defaults to np.random.normal(1.34, 0.26).
+            targetIDs (list, optional): The list of target IDs. Defaults to [].
+        """
         ped = {
             "attributes": {
                 "id": 1,
@@ -75,14 +90,18 @@ class scenarioLoader:
 
         self.dynamicElements_list.append(ped)
 
-
     def save(self, output_dir, scienario_name):
+        """Save the scenario into a vadere scenario file as json format.
+
+        Args:
+            output_dir (string): The directory of the output file.
+            scienario_name (string): Name of the output scenario.
+        """
 
         self.scenario_file_dict["name"] = scienario_name
-        
+
         output_file = os.path.join(output_dir, scienario_name+".scenario")
 
         with open(output_file, "w") as output_scenario:
             json.dump(self.scenario_file_dict, output_scenario, indent=4)
             print("The scenario is saved at", output_dir)
-
